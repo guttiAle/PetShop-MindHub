459e1dd3d66026992ff8d5725c6ba9ca5d830323
Facundo Rojas <facuroja@hotmail.com>
2023-03-19 20:29:09 -0300

    Cambios de estilo farmacia

--- assets/scripts/farmacia.js
+++ assets/scripts/farmacia.js
@@ -1,27 +1,25 @@
 const { createApp } = Vue
 const app = createApp({
-    data(){
-        return{
-            tarjetasFarmacia : []
+    data() {
+        return {
+            tarjetasFarmacia: []
         }
     },
-    created(){
-        fetch( 'https://mindhub-xj03.onrender.com/api/petshop' )
-            .then( response => response.json() )
-            .then( data => { 
+    created() {
+        fetch('https://mindhub-xj03.onrender.com/api/petshop')
+            .then(response => response.json())
+            .then(data => {
                 console.log(data);
                 let separadorPorCategoria = data.filter(elemento => elemento.categoria == "farmacia")
                 this.tarjetasFarmacia = separadorPorCategoria
                 console.log(separadorPorCategoria);
             }
             )
-            // .catch( err => console.log( err ) )      
+        // .catch( err => console.log( err ) )      
     },
-    // computed : {
-    //     filtro(){
-    //         this.eventosFiltrados = this.eventos.filter(evento => evento.name.toLowerCase().includes(this.valorBusqueda.toLowerCase()))
-    //     }
-    // }
+    computed: {
+
+    }
 })
 
 app.mount('#app')
