51e7b30a36880dae5e49d55cd48c41b0048d326b
Agustin Martinez <gutin97agustin.martinez@gmail.com>
2023-03-20 20:52:36 -0300

    Agregand Details, carrito de compra y filtro de busqueda

--- assets/scripts/farmacia.js
+++ assets/scripts/farmacia.js
@@ -2,23 +2,55 @@ const { createApp } = Vue
 const app = createApp({
     data() {
         return {
-            tarjetasFarmacia: []
+            tarjetasFarmacia: [],
+            favoritos : [],
+            productosFiltrados : [],
+            valorBusqueda : '',
+            listaModalDetalles : []
         }
     },
     created() {
         fetch('https://mindhub-xj03.onrender.com/api/petshop')
             .then(response => response.json())
             .then(data => {
-                console.log(data);
                 let separadorPorCategoria = data.filter(elemento => elemento.categoria == "farmacia")
                 this.tarjetasFarmacia = separadorPorCategoria
-                console.log(separadorPorCategoria);
+                this.productosFiltrados = this.tarjetasFarmacia
+                this.getData()
             }
             )
-        // .catch( err => console.log( err ) )      
+            .catch( err => console.log( err ) )
+            this.favoritos = JSON.parse( localStorage.getItem('favoritos') ) || []      
     },
-    computed: {
-
+    methods: {
+        borrarFavoritos(){
+            this.favoritos = []
+        },
+        mostrarDetails(valor){
+            // console.log(valor);
+            for (let i of this.tarjetasFarmacia){
+                if(valor == i.producto){
+                    this.listaModalDetalles = i
+                }
+            }
+        }
+    },
+    computed : {
+        filtro(){
+            this.productosFiltrados = this.tarjetasFarmacia.filter(tarjeta => tarjeta.producto.toLowerCase().includes(this.valorBusqueda.toLowerCase()))
+        },
+        handleFav(){
+            localStorage.setItem( 'favoritos', JSON.stringify( this.favoritos ) )
+        },
+        sumarPrecios(){
+            let contador = 0
+            for (let i of this.tarjetasFarmacia){
+                if(this.favoritos.includes(i.producto)){
+                    contador += i.precio
+                }
+            }
+            return contador
+        },
     }
 })
 
